e56aaabadf144c7f2da10234f2f9c97b39b95e97
Daryl Cimafranca <cimafrancad45@gmail.com>
2018-06-09 10:57:49 -0400

    updated the queryURL api key in the app.js and jquery on the html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,15 @@
 //variables for the search parameters
-var searchInput;
+var searchInput = $("#searchTerm").attr("searchInput");
 var startYear;
 var endYear;
 var recordNum;
 
+function searchTerm(input){
+    var searchInput = input
+}; 
+
 //API + AJAX query
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json/";
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 queryURL += '?' + $.param({
     'api-key': "e5e45bfc549a45c4a54e70b4945d102b",
     'q': searchInput,
@@ -17,5 +21,7 @@ $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function(article){
-    console.log(article)
+    var results = article.data
+    var articleDiv = $("<div>");
+    console.log(results)
 });
